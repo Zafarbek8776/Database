a8a628cb47b9dc0a0d0b54970fc613d1f0d59a34
zafar <rajabboyevzafar6@gmail.com>
2025-11-28 19:52:06 +0500

    a

--- basic_querying/Insert exercise.sql
+++ basic_querying/Insert exercise.sql
@@ -1,2 +1,8 @@
 INSERT INTO film (title, length, rental_rate, language_id)
 VALUES ('THE UNKNOWN ENGINEER', 120, 3.99, 1);
+
+-- 3 --
+INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id)
+VALUES (NOW(), 10, 5, 1);
+
+
