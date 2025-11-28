15fffed5617a48445236332a04089d748417476c
zafar <rajabboyevzafar6@gmail.com>
2025-11-28 19:23:13 +0500

    play

---
+++ basic_querying/Insert exercise.sql
@@ -0,0 +1,2 @@
+INSERT INTO film (title, length, rental_rate, language_id)
+VALUES ('THE UNKNOWN ENGINEER', 120, 3.99, 1);
--- basic_querying/intro.sql
+++ basic_querying/intro.sql
@@ -1,1 +1,5 @@
 select * from film;
+ --2--
+ INSERT INTO film (title, length, rental_rate, language_id)
+VALUES ('THE UNKNOWN ENGINEER', 120, 3.99, 1);
+
